a94cb3dbee3ae75e71dfed695a32e76c930960b3
Heriberto Perez <heriberto.perez@crowdint.com>
2015-01-24 17:25:23 -0600

    Changes GORP for GORM ORM for GO
    - Uses render.v1 package instead of json enconding

--- main.go
+++ main.go
@@ -1,65 +1,62 @@
 package main
 
 import (
-  "database/sql"
   "net/http"
   //"os"
   "github.com/codegangsta/negroni"
-  "github.com/coopernurse/gorp"
-  "github.com/heridev/go_autoresponder_api/controllers/subscribers"
+  //"github.com/heridev/go_autoresponder_api/controllers/subscribers"
   "github.com/heridev/go_autoresponder_api/models/subscriber"
-  _ "github.com/lib/pq"
   "github.com/gorilla/mux"
+  "github.com/jinzhu/gorm"
+  _ "github.com/lib/pq"
+  "gopkg.in/unrolled/render.v1"
 )
 
-var dbmap *gorp.DbMap
 var dberr error
 
-func init() {
-  dbmap, dberr = InitDb()
-  PanicIf(dberr)
+type DBHandler struct {
+  db *gorm.DB
+  r  *render.Render
+}
+
+func InitDb() gorm.DB {
+  // connect to db using standard Go database/sql API
+  db, err := gorm.Open("postgres", "dbname=hmail sslmode=disable")
+  PanicIf(err)
+
+  db.CreateTable(subscriber.Subscriber{})
+  return db
 }
 
 func main() {
-  r := CreateHandler(CreateDbMapHandlerToHTTPHandler(dbmap))
+  db := InitDb()
+  defer db.Close()
+
+  // using the gopkg.in/unrolled/render.v1 library
+  r := render.New(render.Options{})
+  h := DBHandler{db: &db, r: r}
+
+  router := CreateRoutes(&h)
+
   n := negroni.New()
-  n.UseHandler(r)
+  n.UseHandler(router)
   n.Run(":" + "3000")
 }
 
-type HandlerFunc func(w http.ResponseWriter, r *http.Request)
-type DbMapHandlerFunc func(dbmap *gorp.DbMap, w http.ResponseWriter, r *http.Request)
-type DbMapHandlerToHTTPHandlerHOF func(f DbMapHandlerFunc) HandlerFunc
-
-func CreateDbMapHandlerToHTTPHandler(dbmap *gorp.DbMap) DbMapHandlerToHTTPHandlerHOF {
-  return func(f DbMapHandlerFunc) HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-      f(dbmap, w, r)
-    }
-  }
-}
-
-func CreateHandler(f DbMapHandlerToHTTPHandlerHOF) *mux.Router {
+func CreateRoutes(h *DBHandler) *mux.Router {
   r := mux.NewRouter()
-  r.HandleFunc("/subscribers", f(subscribers.GetAll)).Methods("GET")
+  r.HandleFunc("/subscribers",  h.subscribersIndexHandler).Methods("GET")
   return r
 }
 
-func InitDb() (*gorp.DbMap, error) {
-  // connect to db using standard Go database/sql API
-  db, err := sql.Open("postgres", "dbname=hmail sslmode=disable")
-  PanicIf(err)
-
-  // construct a gorp DbMap
-  dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
-  dbmap.AddTableWithName(subscriber.Subscriber{}, "subscribers").SetKeys(true, "id")
-
-  err = dbmap.CreateTablesIfNotExists()
-  if err != nil {
-    return nil, err
+func (h *DBHandler) subscribersIndexHandler(w http.ResponseWriter, req *http.Request) {
+  var subscribers []subscriber.Subscriber
+  h.db.Find(&subscribers)
+  if subscribers == nil {
+    h.r.JSON(w, http.StatusOK, nil) // If we have no subscribers, just return an empty array, instead of null.
+  } else {
+    h.r.JSON(w, http.StatusOK, &subscribers)
   }
-
-  return dbmap, nil
 }
 
 func PanicIf(err error) {
@@ -67,4 +64,3 @@ func PanicIf(err error) {
     panic(err)
   }
 }
-
--- utils/utils.go
+++ utils/utils.go
@@ -1,34 +1,9 @@
 package utils
 
 import (
-  "database/sql"
-  "github.com/coopernurse/gorp"
   "fmt"
   "net/http"
 )
-
-func CreateTableWithID(tableName string, entity interface{}, incrementable bool) (*gorp.DbMap, error) {
-  db, err := sql.Open("postgres", "dbname=hmail sslmode=disable")
-  if err != nil {
-    return nil, err
-  }
-
-  // construct a gorp DbMap
-  dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
-
-  // add a table, setting the table name to 'posts' and
-  // specifying that the Id property is an auto incrementing PK
-  dbmap.AddTableWithName(entity, tableName).SetKeys(incrementable, "id")
-
-  // create the table. in a production system you'd generally
-  // use a migration tool, or create the tables via scripts
-  err = dbmap.CreateTablesIfNotExists()
-  if err != nil {
-    return nil, err
-  }
-
-  return dbmap, nil
-}
 
 func WriteOkResponse(w http.ResponseWriter, httpStatus int, body []byte) {
   w.WriteHeader(httpStatus)
